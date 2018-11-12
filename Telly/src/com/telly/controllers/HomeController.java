84f0a669fcafd75a91c09c4c61ccf3678c495820
mossab12 <mossabinfo@gmail.com>
2018-11-12 09:20:48 +0100

    Security

--- Telly/src/com/telly/controllers/HomeController.java
+++ Telly/src/com/telly/controllers/HomeController.java
@@ -11,5 +11,15 @@ public class HomeController {
 	public String showHome() {
 		
 		return "home";
-	}	
+	}
+    
+    @RequestMapping("/login")
+    public String showLogin() {
+        return "login";
+    }
+    
+    @RequestMapping("/loggedout")
+    public String showLogout() {
+        return "loggedout";
+    }
 }
